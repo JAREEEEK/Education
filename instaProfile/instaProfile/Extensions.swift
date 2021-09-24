182ac1bba34b9cbe4036387f8da8e99151b832f9
Anemiaaa <diangeloq@gmail.com>
2021-09-24 15:01:27 +0300

    6

--- instaProfile/instaProfile/Extensions.swift
+++ instaProfile/instaProfile/Extensions.swift
@@ -49,3 +49,9 @@ extension UIViewController {
         return instantiateFromNib()
     }
 }
+
+extension UIView {
+    class func fromNib<T: UIView>() -> T {
+        return Bundle(for: T.self).loadNibNamed(String(describing: T.self), owner: nil, options: nil)![0] as! T
+    }
+}
